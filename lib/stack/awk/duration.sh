5ba2f8ed55df556992f717a583c863b8d742c713
ufopilot <xgadme2@gamil.com>
2022-07-20 23:15:41 +0200

    init

--- lib/stack/awk/duration.sh
+++ lib/stack/awk/duration.sh
@@ -1,5 +1,6 @@
 
 awk -F '[\\[\\]=,]' '/TaskManager/{tsp=$2}/duration/{print tsp"\t"$5}' duration
+awk -v RS= -F'[][,=]+' '$0=$2"\t"$7' duration
 # awk -F '[=,]' ' 
 #     /duration=/{
 #         printf "%s.%s\t%s\n",strftime("%d.%m.%y %H:%M:%S",$7/1000,1),substr($7,length($7)-2),$5
